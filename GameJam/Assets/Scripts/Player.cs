99a5df767f98cdfdf5452030cc870f1043c86c8f
BlueJayBird11 <51869746+BlueJayBird11@users.noreply.github.com>
2024-04-13 10:34:20 -0500

    Added physics material to player and changed movement system

--- GameJam/Assets/Scripts/Player.cs
+++ GameJam/Assets/Scripts/Player.cs
@@ -6,20 +6,32 @@ public class Player : MonoBehaviour
 {
     public float appliedForce = 20;
     public float runForce = 3;
+    public float damping;
     private Rigidbody2D rb;
     private bool goRight;
     private bool goLeft;
+    private bool isGrounded;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
         goRight = false;
         goLeft = false;
+        isGrounded = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if ( rb.velocity.y == 0)
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded= false;
+        }
         if (goLeft && !goRight)
         {
             rb.AddForce(Vector3.left * runForce * Time.deltaTime, ForceMode2D.Force);
@@ -28,11 +40,18 @@ public class Player : MonoBehaviour
         {
             rb.AddForce(Vector3.right * runForce * Time.deltaTime, ForceMode2D.Force);
         }
+        else if (isGrounded)
+        {
+            rb.velocity = Vector2.MoveTowards(rb.velocity, Vector2.zero, damping * Time.deltaTime);
+        }
     }
 
     public void Jump()
     {
-        rb.AddForce(Vector2.up * appliedForce, ForceMode2D.Impulse);
+        if (isGrounded) 
+        { 
+            rb.AddForce(Vector2.up * appliedForce, ForceMode2D.Impulse);
+        }
     }
 
     public void Left(bool act)
